e91413d2ddcc8fa31cd9de54efe0d5999830e017
Anna-1987 <mag260910@gmail.com>
2025-02-28 15:08:20 +0200

    super

--- inheritance.js
+++ inheritance.js
@@ -3,9 +3,9 @@
 // - super()
 
 class Hero{
-    constructor({name = 'hero', xp = 0}={}) {
+    constructor(name = 'hero', xp = 0) {
          this.name = name;
-        this.xp = xp;
+         this.xp = xp;
     }
 
      gainXp(amount) {
@@ -22,16 +22,40 @@ class Hero{
 // !!!!!!***Для того чтоб класс Warrior наследовал сойства из базавого классас Hero......
 
 class Warrior extends Hero{
-    constructor({weapon =''}={}) {
-        super();
+    constructor(name, xp,weapon) {
+        super(name,xp);
         this.weapon = weapon;
+    }
+
+    attack() {
+        console.log(`${this.name} атакует используя ${this.weapon}`)
     }
 
 }
 
-const mango = new Warrior({ name: 'Mango', xp: 1000, weapon:'алебаpда' });
+class Mage extends Hero{
+    constructor(name, xp, spells = []) {
+        super(name, xp);
+
+        this.spells = spells;
+    }
+
+    cast() {
+        console.log(`${this.name} что-то калдует`);
+    }
+}
+
+const mango = new Warrior( 'Mango',1000, 'алебаpда');
 
 console.log(mango);
-
+mango.attack();
 mango.gainXp(1000);
+console.log(mango);
+
+const poly = new Mage('Poly', 500, ['феарбол']);
+
+console.log(poly);
+poly.cast();
+poly.gainXp(2000);
+console.log(poly);
 
