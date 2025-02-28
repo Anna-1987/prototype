d46f61a9c99f6c49f45e58c69d63dc49be5463a5
Anna-1987 <mag260910@gmail.com>
2025-02-28 15:31:04 +0200

    classes

--- inheritance.js
+++ inheritance.js
@@ -3,7 +3,7 @@
 // - super()
 
 class Hero{
-    constructor(name = 'hero', xp = 0) {
+    constructor({name = 'hero', xp = 0}={}) {
          this.name = name;
          this.xp = xp;
     }
@@ -22,8 +22,8 @@ class Hero{
 // !!!!!!***Для того чтоб класс Warrior наследовал сойства из базавого классас Hero......
 
 class Warrior extends Hero{
-    constructor(name, xp,weapon) {
-        super(name,xp);
+    constructor({ weapon , ...restProps}) {
+        super(restProps);
         this.weapon = weapon;
     }
 
@@ -33,9 +33,21 @@ class Warrior extends Hero{
 
 }
 
+class Berserk extends Warrior{
+    constructor({ warcry, ...restProps } = {}) {
+        super(restProps);
+
+        this.warcry = warcry;
+    }
+    babyRage() {
+        console.log(this.warcry);
+    }
+}
+
+
 class Mage extends Hero{
-    constructor(name, xp, spells = []) {
-        super(name, xp);
+    constructor({ spells, ...restProps }) {
+        super(restProps);
 
         this.spells = spells;
     }
@@ -45,17 +57,24 @@ class Mage extends Hero{
     }
 }
 
-const mango = new Warrior( 'Mango',1000, 'алебаpда');
+const ajax = new Berserk({ name: 'Ajax', xp: 700, weapon: 'exe', warcry: 'waaaaaaah' });
+console.log(ajax);
+ajax.babyRage();
+ajax.attack();
+ajax.gainXp(300);
+console.log(ajax);
 
-console.log(mango);
-mango.attack();
-mango.gainXp(1000);
-console.log(mango);
+const mango = new Warrior({name: 'Mango', xp: 1000, weapon:'алебаpда'});
 
-const poly = new Mage('Poly', 500, ['феарбол']);
+// console.log(mango);
+// mango.attack();
+// mango.gainXp(1000);
+// console.log(mango);
 
-console.log(poly);
-poly.cast();
-poly.gainXp(2000);
-console.log(poly);
+const poly = new Mage({name:'Poly',xp: 500,spells: ['феарбол']});
+
+// console.log(poly);
+// poly.cast();
+// poly.gainXp(2000);
+// console.log(poly);
 
